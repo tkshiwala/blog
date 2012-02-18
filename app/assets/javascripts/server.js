8f234f7f75a6aa236f961b7586104b95629e5907
Tikky Shiwala <tkshiwala@gmail.com>
2012-02-18 17:34:08 -0500

    Added server.js and less file

---
+++ app/assets/javascripts/server.js
@@ -0,0 +1,13 @@
+/**
+ * Created by JetBrains RubyMine.
+ * User: Tikky
+ * Date: 2/18/12
+ * Time: 5:06 PM
+ * To change this template use File | Settings | File Templates.
+ */
+//var http = require('http');
+//http.createServer(function (req, res) {
+//  res.writeHead(200, {'Content-Type': 'text/plain'});
+//  res.end('Hello World\n');
+//}).listen(8124, "127.0.0.1");
+//console.log('Server running at http://127.0.0.1:8124/');
